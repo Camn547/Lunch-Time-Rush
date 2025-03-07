f3f0aa66c3933950fea9a075f1a8333ca9c51364
Camn547 <Camomichael1@icloud.com>
2025-03-06 18:02:26 -0700

    got var subject to set correctly when pressing one of the subject buttons

--- LTR.js
+++ LTR.js
@@ -1,22 +1,54 @@
 $(document).ready(function(){
-    let subject = "";
-    $("h3").html(`${subject}`)
     
+    // arrays for all questions of each subject
+    const QEnglish = [
+        ""
+    ];
+
+    const QMath = [
+        // x = 7
+        'Solve for x, "4 (x + 5) = 48"',
+
+        // 256
+        'What is 16 ^ 2?',
+
+        // 7.5
+        'Solve: 6 + 3 * (5 - 3) ÷ 4.',
+
+        // 11/8 or 1 3/8
+        'What is 3/4 + 5/8 simplified?',
+
+        // 7.43
+        'Add 4.75 and 2.68.'
+    ];
+
+    const QHistory = [
+        ""
+    ];
+
+    const QScience = [
+        ""
+    ];
+
+    let subject;
     $(".English").on('click', function(){
-        let subject = "English"
-        return subject
+        subject = "English"
     })
+
     $(".Math").on('click', function(){
-        let subject = "Math"
-        return subject
+        subject = "Math"
     })
+
     $(".History").on('click', function(){
-        let subject = "History"
-        return subject
+        subject = "History"
     })
+
     $(".Science").on('click', function(){
-        let subject = "Science"
-        return subject
+        subject = "Science"
     })
+
+    setInterval(function(){
+        console.log(subject)
+    },1000);
 
 })
