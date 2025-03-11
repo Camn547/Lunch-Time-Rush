a2398ffa01609c15737d7df5bbac271c397fef61
Camn547 <Camomichael1@icloud.com>
2025-03-10 18:22:41 -0700

    worked on fixing game type

--- LTR.js
+++ LTR.js
@@ -1,67 +1,47 @@
 $(document).ready(function(){
     
-    // arrays for all questions of each subject
-    const QEnglish = [
-        // Metaphor
-        'What kind of figurative language is used in this sentence? "life is a box of chocolates, you never know what you are gonna get"',
-
-        // Edgar Allan Poe
-        'Who wrote "The Tell-Tale Heart"?',
-        
-        // Hamlet
-        'which shakespeare play is "The Lion King" inspired by',
-
-        //
-        ''
-    ];
-
-    const QMath = [
-        // x = 7
-        'Solve for x, "4 (x + 5) = 48"',
-
-        // 256
-        'What is 16 ^ 2?',
-
-        // 7.5
-        'Solve: 6 + 3 * (5 - 3) ÷ 4.',
-
-        // 11/8 or 1 3/8
-        'What is 3/4 + 5/8 simplified?',
-
-        // 7.43
-        'Add 4.75 and 2.68.',
-
-        // 126
-        'Find the area of a rectangle with a length of 18 cm and a width of 7 cm.'
-    ];
-
-    const QHistory = [
-        ""
-    ];
-
-    const QScience = [
-        ""
-    ];
-
-    let subject;
-    $(".English").on('click', function(){
-        subject = "English"
+    let difficulty;
+    let milliseconds = 0;
+    let seconds = 0;
+    let minutes = 0;
+    $(".Easy").on('click', function(){
+        difficulty = "Easy"
     })
 
-    $(".Math").on('click', function(){
-        subject = "Math"
+    $(".Medium").on('click', function(){
+        difficulty = "Medium"
     })
 
-    $(".History").on('click', function(){
-        subject = "History"
+    $(".Hard").on('click', function(){
+        difficulty = "Hard"
     })
 
-    $(".Science").on('click', function(){
-        subject = "Science"
-    })
+    function startTimer() {
+        interval = setInterval(function() {
+            milliseconds += 1;
+            if (milliseconds >= 100) {
+                milliseconds = 0;
+                seconds += 1;
+            }
+            if (seconds >= 60) {
+                seconds = 0;
+                minutes += 1;
+            }
+            updateDisplay();
+        }, 10);
+    }
 
-    setInterval(function(){
-        console.log(subject)
-    },1000);
+    function stopTimer() {
+        clearInterval(interval);
+    }
+
+    function updateDisplay() {
+        let ms = milliseconds.toString().padStart(2, "0");
+        let sec = seconds.toString().padStart(2, "0");
+        let min = minutes.toString().padStart(2, "0");
+
+        $("h3").html(`${min}:${sec}:${ms}`);
+    }
+    updateDisplay();
 
 })
