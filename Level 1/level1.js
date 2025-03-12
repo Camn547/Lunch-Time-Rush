20adeaf4ace8686cc654d956bb810252138c1add
angie-cote927 <mcote7243@gmail.com>
2025-03-11 18:14:27 -0700

    Started Level 1
    Figured out how to make it look like cards are flipping. Started to try and make the cards positions randomized, but couldn't quite figure it out.

--- Level 1/level1.js
+++ Level 1/level1.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-    $(".front").hide(1)
-    $(".back").on("click", function(){
-        $(".back").animate({perspective: "-800px"}, 1000)
-    })
-})
+// $(document).ready(function(){
+//     $(".front").hide(1)
+//     $(".back").on("click", function(){
+//         $(".front").show(1)
+//         $(".back").animate({width: "1px", marginLeft: "7rem"}, 1000)
+//         $(".front").delay(1000).animate({width: "30rem", marginLeft: "-7.5rem"}, 1000)
+//     })
+// })
+
+cards = [["10rem", "20rem"], ["10rem", "45rem"], ["10rem", "70rem"], ["30rem", "20rem"], ["30rem", "45rem"], ["30rem", "70rem"]]
+
+divs = $("div")
+
+
+Math.round(Math.random() * cards.length())
