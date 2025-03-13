82f22e1b289bb87ed37d8deb708d1b308c524145
Camn547 <Camomichael1@icloud.com>
2025-03-12 17:36:31 -0700

    added kitty and score

--- LTR.js
+++ LTR.js
@@ -3,12 +3,24 @@ $(document).ready(function(){
     let milliseconds = 0;
     let seconds = 0;
     let minutes = 0;
-
+    let score = 1000;
 
     $("button").on("click", function(){
         $(".ready").fadeOut(150)
         startTimer();
+        startScore();
     })
+
+    $(".kitty").on("click", function(){
+        $("img").fadeOut(2000);
+    })
+
+    function startScore(){
+        interval2 = setInterval(function(){
+            score -= 1;
+            updateScore();
+        }, 75);
+    }
 
     function startTimer() {
         interval = setInterval(function() {
@@ -29,13 +41,20 @@ $(document).ready(function(){
         clearInterval(interval);
     }
 
+    function updateScore(){
+        let sc = score.toString().padStart("1000");
+
+        $(".score p").html(`Score: ${sc}`);
+    }
+
     function updateDisplay() {
         let ms = milliseconds.toString().padStart(2, "0");
         let sec = seconds.toString().padStart(2, "0");
         let min = minutes.toString().padStart(2, "0");
 
-        $("h3").html(`${min}:${sec}:${ms}`);
+        $("h3").html(`Time: ${min}:${sec}:${ms}`);
     }
     updateDisplay();
+    updateScore();
 
 })
