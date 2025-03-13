48d98f06328e5a144c830da370709b0d63417a10
angie-cote927 <mcote7243@gmail.com>
2025-03-12 18:22:08 -0700

    Click Detection
    Made it so it detects when you click and detects what id you clicked and adds and removes matches accordingly.

--- Level 1/level1.js
+++ Level 1/level1.js
@@ -5,7 +5,13 @@ $(document).ready(function(){
     //     $(".back").animate({width: "1px", marginLeft: "7rem"}, 1000)
     //     $(".front").delay(1000).animate({width: "30rem", marginLeft: "-7.5rem"}, 1000)
     // })
-    cards = [["8rem", "16rem"], ["8rem", "41rem"], ["8rem", "66rem"], ["28rem", "16rem"], ["28rem", "41rem"], ["28rem", "66rem"]]
+    const cards = [["8rem", "16rem"], ["8rem", "41rem"], ["8rem", "66rem"], ["28rem", "16rem"], ["28rem", "41rem"], ["28rem", "66rem"]]
+
+    var treats = 0
+    var cat = 0
+    var mouse = 0
+    var yarn = 0
+    match = 0
 
 
     var randomIndex = Math.floor(Math.random() * cards.length)
@@ -21,6 +27,37 @@ $(document).ready(function(){
         if (randomIndex >= cards.length) {
             randomIndex = 0
         }
+    })
+
+    $(".card").on("click", function(){
+        if ($(this).attr("id") == "cat"){
+            cat++
+            mouse = 0
+            yarn = 0
+        }
+        if ($(this).attr("id") == "mouse"){
+            mouse++
+            cat = 0
+            yarn = 0
+        }
+        if ($(this).attr("id") == "yarn"){
+            yarn++
+            cat = 0
+            mouse = 0
+        }
+        if (cat == 2){
+            match++
+            treats += 15
+        }
+        if (mouse == 2){
+            match++
+            treats += 15
+        }
+        if (yarn == 2){
+            match++
+            treats += 15
+        }
+        console.log(cat, mouse, yarn, match, treats)
     })
 })
 
