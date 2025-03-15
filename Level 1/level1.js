669e9cfa5bcbfc86254540ef308c01fd3c51bd1e
Camn547 <Camomichael1@icloud.com>
2025-03-14 18:21:25 -0700

    h

--- Level 1/level1.js
+++ Level 1/level1.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
     //     $(".back").animate({width: "1px", marginLeft: "7rem"}, 1000)
     //     $(".front").delay(1000).animate({width: "30rem", marginLeft: "-7.5rem"}, 1000)
     // })
+
+    $(".card").delay(6000).animate({width: "1px", marginLeft: "7rem"}, 1000)
+
     $(".celebrate").hide(1)
     const cards = [["8rem", "16rem"], ["8rem", "41rem"], ["8rem", "66rem"], ["28rem", "16rem"], ["28rem", "41rem"], ["28rem", "66rem"]]
 
@@ -17,8 +20,12 @@ $(document).ready(function(){
 
     var randomIndex = Math.floor(Math.random() * cards.length)
 
+    function stopTimer() {
+        clearInterval(interval);
+    }
 
-    $("img").each(function(index, element) {
+
+    $("#game img").each(function(index, element) {
         console.log(randomIndex)
         $(element).css('position', 'absolute')
         console.log(cards)
@@ -32,6 +39,7 @@ $(document).ready(function(){
 
     $(".card").on("click", function(){
         possible_match++
+        $(this).addClass("lol")
         if (possible_match == 1){
             choice1 = $(this).attr("id")
             $(this).css('border','2px solid yellow')
@@ -55,8 +63,15 @@ $(document).ready(function(){
             $(".treatsCounter").html(`<h1>Treats: ${treats}</h1>`)
             if (match == 3) {
                 $(".celebrate").show(1)
+                stopTimer();
             }
             $(this).addClass("hue-rotate")
+        }
+        if (possible_match == 2 && choice1 != choice2) {
+            possible_match = 0
+            choice1 = undefined
+            choice2 = undefined
+            $(".card").removeClass("lol")
         }
     })
 
